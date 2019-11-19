eb2ddb244a6d4eb76c79109af97eeb9c3810690a
Tai Corraggio <theresawilkin@Theresas-MacBook-Air.local>
2019-11-19 11:29:49 -0500

    navigation and authentication

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import Home from './components/Home';
+import HomePage from './pages/HomePage';
+import UsersListPage from './pages/UsersListPage';
+import App from './App';
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-    </div>
-  );
-};
+export default [
+  {
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: "/",
+        exact: true
+      },
+      {
+        ...UsersListPage,
+        path: "/users"
+      }
+    ]
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
+import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
+import Routes from './client/Routes';
 
 const app = express();
 
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    }
+}));
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore(req);
+
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
