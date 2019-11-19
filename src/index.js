8d45bd892c520055b9b8d9999f517d0921b06925
Tai Corraggio <theresawilkin@Theresas-MacBook-Air.local>
2019-11-19 18:04:40 -0500

    completed tutorial

--- src/index.js
+++ src/index.js
@@ -46,6 +46,9 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+const PORT = process.env.PORT || 3000;
+     app.listen(PORT, () => {
+     console.log(`Listening on port ${PORT}`);
+}).on('error', (err) => {
+     console.log(`Error Code: ${err.code}`);
 });
